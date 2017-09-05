428233f423729ab67d8dd7a41802c18aa1f4ec7f
Angie the Fairytale <konzeptmeister@gmail.com>
2017-09-05 23:53:12 +0200

    OPACITY_SELECT = 1

--- src/component.js
+++ src/component.js
@@ -383,7 +383,7 @@ const BubbleMapComponent = Component.extend("bubblemap", {
 
     const OPACITY_HIGHLT = 1.0;
     const OPACITY_HIGHLT_DIM = 0.3;
-    const OPACITY_SELECT = this.model.marker.opacityRegular;
+    const OPACITY_SELECT = 1.0;
     const OPACITY_REGULAR = this.model.marker.opacityRegular;
     const OPACITY_SELECT_DIM = this.model.marker.opacitySelectDim;
 
